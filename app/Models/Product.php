a99a60b6abc056f651c5a2bd0cab7fbcd03424f0
Omar Abi Farraj <omar.7tech@gmail.com>
2026-08-04 22:13:32 +0300

    ere

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,13 +9,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Sluggable\Attributes\Sluggable;
 
 #[Sluggable(from: 'title', to: 'slug')]
 #[Guarded(['id'])]
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     /** @use HasFactory<ProductFactory> */
     use HasFactory, InteractsWithMedia;
