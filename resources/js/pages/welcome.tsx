567091ac410c1d263ca6bb75020f7c2041c9bb59
Omar Abi Farraj <omar.7tech@gmail.com>
2026-08-04 12:33:29 +0300

    make the home page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,11 +1,24 @@
 import { Head } from '@inertiajs/react';
+import { Button } from '@/components/ui/button';
 
 export default function Welcome() {
     return (
         <>
             <Head title="Welcome" />
-            <div>
-                <h1>Welcome</h1>
+            <div className="flex min-h-svh flex-col items-center justify-center gap-10 px-6">
+                <img
+                    src="/logos/pine-logo-horizontal.svg"
+                    alt="Pine"
+                    className="w-full max-w-sm"
+                />
+                <div className="flex flex-wrap items-center justify-center gap-4">
+                    <Button size="lg" className="h-11 px-8 text-base">
+                        Menu
+                    </Button>
+                    <Button size="lg" className="h-11 px-8 text-base">
+                        Reserve your spot
+                    </Button>
+                </div>
             </div>
         </>
     );
